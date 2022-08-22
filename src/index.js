2c5903c58f89fa7cd78bbf7d60fb96bbcf22bd83
Margaret Kenda <margaret.kenda@yahoo.com>
2022-08-22 16:19:25 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// document.addEventListener("DOMContentLoaded", () => {
+//   document.querySelector('form').addEventListener('submit', (e) =>{
+//     e.preventdefault()
+//   })
+// });
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault()
+  const inputfield = document.getElementById("new-task-description").value;
+  document.getElementById('tasks').innerHTML += `
+     <li>${inputfield}</li>
+
+
+`
+  document.getElementById("new-task-description").value = ''
+
+})
+
+
+
 });
